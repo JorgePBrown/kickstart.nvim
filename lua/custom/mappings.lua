7313732e667ce5c80eaad4d34e892711a9b26cb2
Jorge Brown <j.brown@moovit-sp.com>
2024-03-01 09:47:20 +0100

    Updates

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,8 +2,12 @@ vim.keymap.set('n', '<C-s>', ':update<Enter>', { desc = "[S]ave the current buff
 
 vim.keymap.set('n', '<leader>fe', ':E<Enter>', { desc = "[F]ile [E]xplorer" })
 
-vim.keymap.set('n', '<leader>bd', ':bdelete<Enter>', { desc = "[B]uffer [d]elete" })
+-- vim.keymap.set('n', '<leader>bd', ':bdelete<Enter>', { desc = "[B]uffer [d]elete" })
 
 vim.keymap.set('v', 's', 'y:s/<C-r>0//g<left><left>', { desc = "[S]ubstitute highlighted word" })
 
 vim.keymap.set('n', '<leader>gb', ':Git blame<Enter>', { desc = "[G]it [B]lame" })
+
+vim.keymap.set('n', '<leader>lr', ':LspRestart<Enter>', { desc = "[L]SP [R]lame" })
+
+vim.keymap.set('n', '<leader>cd', ":echo getreg('#')<Enter>", { desc = "[C]urrent [D]irectory" })
